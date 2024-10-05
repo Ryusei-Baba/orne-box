97003a68cb22fdcd666881e1748730e15506736a
Ryusei Baba <babaryusei.kw@gmail.com>
2024-10-06 01:41:17 +0900

    orne_box_lidar_with_mirrorと連携

--- orne_box_bringup/launch/include/mirror_lidar.launch.py
+++ orne_box_bringup/launch/include/mirror_lidar.launch.py
@@ -19,10 +19,10 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch.actions import (DeclareLaunchArgument, EmitEvent, RegisterEventHandler)
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 from launch.events import matches_action
-from launch_ros.actions import LifecycleNode
+from launch_ros.actions import LifecycleNode, Node
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
@@ -30,14 +30,19 @@ from lifecycle_msgs.msg import Transition
 def generate_launch_description():
 
     # パラメータファイルのパス設定
-    config_file_path = os.path.join(
+    config_file_path_urg = os.path.join(
         get_package_share_directory('orne_box_bringup'),
-        'config/urg/',
-        'params_mirror_lidar.yaml'
+        'config/urg/params_mirror_lidar.yaml'
+    )
+
+    config_file_path_lidar = os.path.join(
+        get_package_share_directory('orne_box_lidar_with_mirror'),
+        'config',
+        'lidar_with_mirror_control.yaml'
     )
 
     # パラメータファイルのロード
-    with open(config_file_path, 'r') as file:
+    with open(config_file_path_urg, 'r') as file:
         config_params = yaml.safe_load(file)['urg_node2']['ros__parameters']
 
     # urg_node2をライフサイクルノードとして起動
@@ -85,10 +90,33 @@ def generate_launch_description():
         condition=IfCondition(LaunchConfiguration('auto_start')),
     )
 
+    # コントローラーやノードの起動
+    joint_state_broadcaster_spawner = Node(
+        package='ros2_control_node',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+        output='screen'
+    )
+
+    lidar_with_mirror_controller_spawner = Node(
+        package='ros2_control_node',
+        executable='spawner',
+        arguments=['orne_box_lidar_with_mirror_controllers', '--controller-manager', '/controller_manager'],
+        output='screen'
+    )
+
+    merged_data_node = Node(
+        package='orne_box_lidar_with_mirror',
+        executable='merge_measured_data.py',
+        name='merged_data_node',
+        output='screen'
+    )
+
     # パラメータについて
     # auto_start      : 起動時自動でActive状態まで遷移 (default)true
     # node_name       : ノード名 (default)"urg_node2"
     # scan_topic_name : トピック名 (default)"scan" *マルチエコー非対応*
+
     return LaunchDescription([
         DeclareLaunchArgument('auto_start', default_value='true'),
         DeclareLaunchArgument('node_name', default_value='urg_node2'),
@@ -96,5 +124,7 @@ def generate_launch_description():
         lifecycle_node,
         urg_node2_node_configure_event_handler,
         urg_node2_node_activate_event_handler,
+        joint_state_broadcaster_spawner,
+        lidar_with_mirror_controller_spawner,
+        merged_data_node
     ])
-
